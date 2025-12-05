7055ec83473f22798cd10c57fe4756897492a9c2
muhammad <olmospro1978@gmail.com>
2025-12-05 18:15:03 +0500

    ann based config

--- src/main/java/uz/pdp/ProductValidator.java
+++ src/main/java/uz/pdp/ProductValidator.java
@@ -1,6 +1,15 @@
 package uz.pdp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductValidator {
+
+    @Autowired
+    private ProductService productService;
+
+
     public void validate() {
         System.out.println("validate method works");
     }
