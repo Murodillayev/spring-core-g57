2d7efa8ae8087e489a68b3ca85512c66cd338127
muhammad <olmospro1978@gmail.com>
2025-12-24 15:04:13 +0500

    security

---
+++ src/main/java/uz/pdp/repository/impl/RoleRepositoryImpl.java
@@ -0,0 +1,12 @@
+package uz.pdp.repository.impl;
+
+import org.springframework.stereotype.Repository;
+import uz.pdp.repository.RoleRepository;
+
+@Repository
+public class RoleRepositoryImpl implements RoleRepository {
+    @Override
+    public String findById(String id) {
+        return "";
+    }
+}
